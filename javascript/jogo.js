5b7225865b85dc49ed38990d9c3715807e69e0e7
LagAntunes <lag151617@gmail.com>
2022-03-03 20:05:44 -0300

    adicionando animações dos personagens

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -1,14 +1,16 @@
 /* JS do jogo*/
-let fundoGame = document.getElementById("fundoGame");
-let instrucoes = document.getElementById("blocoInstrucoes");
-let botaoComecaJogo = document.getElementById("botao-comeca-jogo");
-
-botaoComecaJogo.addEventListener("click", () => {
-    $(instrucoes).hide();
+    let fundoGame = document.getElementById("fundoGame");
+    let instrucoes = document.getElementById("blocoInstrucoes");
+    let botaoComecaJogo = document.getElementById("botao-comeca-jogo");
 
 
-    $(fundoGame).append(`<div id="jogador"></div>`);
-    $(fundoGame).append(`<div id="amigo"></div>`);
-    $(fundoGame).append(`<div id="inimigo1"></div>`);
-    $(fundoGame).append(`<div id="inimigo2"></div>`);
-})
+/* inicia o jogo com o clique do botão */
+    botaoComecaJogo.addEventListener("click", () => {
+        $(instrucoes).hide();
+
+
+        $(fundoGame).append(`<div id="jogador" class="animacaoJogador"></div>`);
+        $(fundoGame).append(`<div id="amigo" class="animacaoAmigo"></div>`);
+        $(fundoGame).append(`<div id="inimigo1" class="animacaoInimigo1"></div>`);
+        $(fundoGame).append(`<div id="inimigo2" class="animacaoInimigo2"></div>`);
+    })
