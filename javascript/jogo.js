ef255d3e3fa69ca19f49b8ca400349b8a7eb265c
LagAntunes <lag151617@gmail.com>
2022-03-03 19:03:15 -0300

    adicionando os personagens

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -1,1 +1,14 @@
 /* JS do jogo*/
+let fundoGame = document.getElementById("fundoGame");
+let instrucoes = document.getElementById("blocoInstrucoes");
+let botaoComecaJogo = document.getElementById("botao-comeca-jogo");
+
+botaoComecaJogo.addEventListener("click", () => {
+    $(instrucoes).hide();
+
+
+    $(fundoGame).append(`<div id="jogador"></div>`);
+    $(fundoGame).append(`<div id="amigo"></div>`);
+    $(fundoGame).append(`<div id="inimigo1"></div>`);
+    $(fundoGame).append(`<div id="inimigo2"></div>`);
+})
