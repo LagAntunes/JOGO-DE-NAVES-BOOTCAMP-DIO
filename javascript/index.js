a18ed1da7225bcebd01278772da809a483040442
LagAntunes <lag151617@gmail.com>
2022-03-03 18:00:28 -0300

    adicionando funcionalidades ao botão inicial

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+/* JS do botão da página inicial*/
+
+let botaoInicial = document.getElementById("botao-inicial");
+botaoInicial.onclick = () => {
+    event.preventDefault;
+    window.location.href = "../jogo.html";
+}
