2687a58a44470e1d4fedadc0629e0766f357f9f4
LagAntunes <lag151617@gmail.com>
2022-03-05 20:42:10 -0300

    adicionando o primeiro efeito de explosão do jogo

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -24,7 +24,8 @@
         moveJogador();
         moveInimigo1();
         moveInimigo2();
-        moveAmigo()
+        moveAmigo();
+        colisao();
     }
 
 /* teclas de comando do jogador */
@@ -48,6 +49,8 @@
     let velocidade = 5;
     let posicaoY = parseInt(Math.random() * 334);
 
+/* váriáveis de movimentação do ataque do personagem */
+    let podeAtirar = true;
 
 /* função que movimenta o background */
     function moveFundo() {
@@ -74,7 +77,7 @@
         }
 
         if(jogo.pressionou[Tecla.D]) {
-            
+            disparo();
         }
     }
 
@@ -102,11 +105,73 @@
     }
 
 /* função que movimenta o amigo */
-function moveAmigo() {
-    posicaoX = parseInt($(`#amigo`).css(`left`));
-    $(`#amigo`).css(`left`, posicaoX + 1);
+    function moveAmigo() {
+        posicaoX = parseInt($(`#amigo`).css(`left`));
+        $(`#amigo`).css(`left`, posicaoX + 1);
 
-    if(posicaoX > 906) {
-        $(`#amigo`).css(`left`, 0);
+        if(posicaoX > 906) {
+            $(`#amigo`).css(`left`, 0);
+        }
     }
-}
+
+/* função que possibilita o disparo */
+    function disparo() {
+        if(podeAtirar == true) {
+            podeAtirar = false;
+
+            Topo = parseInt($(`#jogador`).css(`top`));
+            posicaoX = parseInt($(`#jogador`).css(`left`));
+            tiroX = posicaoX + 190;
+            TopoTiro = Topo + 37;
+            $(fundoGame).append(`<div id="disparo"></div>`);
+            $(`#disparo`).css(`top`, TopoTiro);
+            $(`#disparo`).css(`left`, tiroX);
+
+            var tempoDisparo = window.setInterval(executaDisparo, 30)
+        }
+
+
+        function executaDisparo() {
+            posicaoX = parseInt($(`#disparo`).css(`left`));
+            $(`#disparo`).css(`left`, posicaoX + 10);
+
+            if(posicaoX > 900) {
+                window.clearInterval(tempoDisparo);
+                tempoDisparo = null;
+                $(`#disparo`).remove();
+                podeAtirar = true;
+            }
+        }
+    }
+
+/* função da colisão */
+    function colisao() {
+        var colisao1 = ($(`#jogador`).collision($(`#inimigo1`)));
+
+        if(colisao1.length > 0) {
+            inimigo1X = parseInt($(`#inimigo1`).css(`left`));
+            inimigo1Y = parseInt($(`#inimigo1`).css(`top`));
+            explosao1(inimigo1X, inimigo1Y);
+
+            posicaoY = parseInt(Math.random() * 334);
+            $(`#inimigo1`).css(`left`, 694);
+            $(`#inimigo1`).css(`top`, posicaoY);
+        }
+    }
+
+/* função da explosão */
+    function explosao1(inimigo1X, inimigo1Y) {
+        $(fundoGame).append(`<div id="explosao1"></div>`);
+        var divDaExplosao1 = $(`#explosao1`);
+        divDaExplosao1.css(`top`, inimigo1Y);
+        divDaExplosao1.css(`left`, inimigo1X);
+        divDaExplosao1.animate({width:200, opacity: 0}, `slow`);
+
+        var tempoDaExplosao1 = window.setInterval(removeExplosao1, 1000);
+
+        function removeExplosao1() {
+            divDaExplosao1.remove();
+            window.clearInterval(tempoDaExplosao1);
+            tempoDaExplosao1 = null;
+        }
+    }
